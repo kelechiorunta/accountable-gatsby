6cc1de4d827bfc52b61808887fe8b5fd0f485a66
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-24 14:48:03 +0100

    Add navigation to header component

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,7 +1,14 @@
 import React from "react"
+import Navigation from "../components/header-navigation"
 
 const Header = () => {
-  return <></>
+  return (
+    <>
+      <header className="px-6 sm:px-10 py-4 lg:py-12 flex flex-wrap items-center absolute w-full z-20">
+        <Navigation />
+      </header>
+    </>
+  )
 }
 
 export default Header
