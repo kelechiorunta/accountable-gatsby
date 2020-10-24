0aa94ab7b280213cbbe501d625ca901ae0bc05c2
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-24 13:27:10 +0100

    Added Gatsby PostCSS plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `Accountable.org.ng`
+    title: `Accountable.org.ng`,
+    description: `Get justice for your family and friends. Report a crime today and get your world accountable.`,
   },
   plugins: [
+    `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          `dm sans\:400,400i,500,500i,700,700i` // you can also specify font weights and styles
+          `dm sans\:400,400i,500,500i,700,700i`, // you can also specify font weights and styles
         ],
-        display: 'swap'
-      }
-    }
-  ]
+        display: "swap",
+      },
+    },
+  ],
 }
