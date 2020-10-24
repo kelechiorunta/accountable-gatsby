81f8011dc54712761ead9ffd774f4ea6e350cc03
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-24 14:05:58 +0100

    Added sub caption to landing screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,10 @@ const HomePage = () => {
           <h1 className="text-theme mb-2 text-3xl sm:text-5xl font-medium leading-none tracking-tightest">
             Accountable.org.ng
           </h1>
+          <h1 className="mb-8 text-gray-800 text-2xl sm:text-4xl font-medium leading-tight tracking-tightest">
+            Hold your government, politicians, institutions, public office
+            holders & citizens accountable…
+          </h1>
         </div>
       </main>
     </Layout>
