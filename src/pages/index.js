eecf80e1f7a72230c33fa94e440db745ed18ca4b
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-24 14:18:52 +0100

    Added featured area and buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Layout from "../layout/layout"
+import Link from "gatsby-link"
 
 const HomePage = () => {
   return (
@@ -13,6 +14,62 @@ const HomePage = () => {
             Hold your government, politicians, institutions, public office
             holders & citizens accountable…
           </h1>
+          <div className="flex flex-col sm:flex-row">
+            <div className="w-full sm:w-1/2 lg:w-1/3 sm:mr-6">
+              <div className="mb-5 hidden sm:visible sm:flex flex-col">
+                <h1 className="text-theme text-2xl font-medium tracking-tightest">
+                  Report A Crime
+                </h1>
+                <p className="text-lg">
+                  Tell us about a crime committed to you
+                </p>
+              </div>
+              <Link
+                className="bg-theme block rounded-lg px-6 py-4 text-white font-medium"
+                to="/report"
+              >
+                <span>Report Crime</span>
+                <div className="float-right">
+                  <svg
+                    width="24"
+                    height="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    clip-rule="evenodd"
+                  >
+                    <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
+                  </svg>
+                </div>
+              </Link>
+            </div>
+            <div className="w-full sm:w-1/2 lg:w-1/3 mt-4 sm:mt-0">
+              <div className="mb-5 hidden sm:visible sm:flex flex-col">
+                <h1 className="text-theme text-2xl font-medium tracking-tightest">
+                  Crimes Database
+                </h1>
+                <p className="text-lg">
+                  See all crimes as reported by every one
+                </p>
+              </div>
+              <Link
+                className="bg-black block rounded-lg px-6 py-4 text-white font-medium"
+                to="/crimes"
+              >
+                <span>See All Crimes</span>
+                <div className="float-right">
+                  <svg
+                    width="24"
+                    height="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    clip-rule="evenodd"
+                  >
+                    <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
+                  </svg>
+                </div>
+              </Link>
+            </div>
+          </div>
         </div>
       </main>
     </Layout>
