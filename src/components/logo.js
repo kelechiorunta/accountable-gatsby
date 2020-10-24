ff4ab01472401580593c4f6b650329c1d8d46c11
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-25 00:24:35 +0100

    Created logo component

---
+++ src/components/logo.js
@@ -0,0 +1,16 @@
+import React from "react"
+import LogoImage from "../images/logo.png"
+
+const Logo = () => {
+  return (
+    <>
+      <img
+        src={LogoImage}
+        width="50"
+        className="absolute top-0 left-0 ml-6 sm:ml-10 mt-4 sm:mt-8"
+      />
+    </>
+  )
+}
+
+export default Logo
