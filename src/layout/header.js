7ea94e2a2822a95653c99ee19ee3baa868ee1c43
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-24 15:02:52 +0100

    Added Get Started button

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,4 +1,5 @@
 import React from "react"
+import GetStartedButton from "../components/get-started-button"
 import Navigation from "../components/navigation"
 
 const Header = () => {
@@ -6,6 +7,7 @@ const Header = () => {
     <>
       <header className="px-6 sm:px-10 py-4 lg:py-12 flex flex-wrap items-center absolute w-full z-20">
         <Navigation />
+        <GetStartedButton />
       </header>
     </>
   )
