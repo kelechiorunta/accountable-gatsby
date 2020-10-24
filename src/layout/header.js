3c7f893cae5a936e8ea94558733f0371583bc74a
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-25 00:25:29 +0100

    Added logo and toggle components

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,13 +1,17 @@
 import React from "react"
 import GetStartedButton from "../components/get-started-button"
+import Logo from "../components/logo"
 import Navigation from "../components/navigation"
+import Toggle from "../components/toggle"
 
 const Header = () => {
   return (
     <>
       <header className="px-6 sm:px-10 py-4 lg:py-12 flex flex-wrap items-center absolute w-full z-20">
+        <Logo />
         <Navigation />
         <GetStartedButton />
+        <Toggle />
       </header>
     </>
   )
