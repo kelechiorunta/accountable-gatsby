4e2779c2d74cddc8d0e49c6deecb38b860d42698
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-24 14:04:34 +0100

    Added h1 caption title to landing screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,17 @@ import React from "react"
 import Layout from "../layout/layout"
 
 const HomePage = () => {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <main className="landing-screen-bg flex h-full items-center px-6 sm:px-12 lg:px-32 z-0">
+        <div className="w-full md:w-9/12 lg:w-9/12 xl:w-8/12 z-10">
+          <h1 className="text-theme mb-2 text-3xl sm:text-5xl font-medium leading-none tracking-tightest">
+            Accountable.org.ng
+          </h1>
+        </div>
+      </main>
+    </Layout>
+  )
 }
 
 export default HomePage
