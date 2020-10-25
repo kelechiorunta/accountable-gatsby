c2df749bd75e5912e4ae207d1425c156e8559885
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-25 01:56:25 +0100

    Added toggleState, toggleClass and toggle method.

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,17 +1,30 @@
-import React from "react"
+import React, { useState } from "react"
 import GetStartedButton from "../components/get-started-button"
 import Logo from "../components/logo"
 import Navigation from "../components/navigation"
 import Toggle from "../components/toggle"
 
 const Header = () => {
+  const [toggleState, setToggleState] = useState(true)
+  const [toggleClass, setToggleClass] = useState("theme-nav-close")
+
+  const toggle = () => {
+    if (toggleState) {
+      setToggleState(!toggleState)
+      setToggleClass("theme-nav-open")
+    } else {
+      setToggleState(!toggleState)
+      setToggleClass("theme-nav-close")
+    }
+  }
+
   return (
     <>
       <header className="px-6 sm:px-10 py-4 lg:py-12 flex flex-wrap items-center absolute w-full z-20">
         <Logo />
-        <Navigation />
+        <Navigation toggleClass={toggleClass} />
         <GetStartedButton />
-        <Toggle />
+        <Toggle onToggle={toggle} />
       </header>
     </>
   )
