92e6e567b81390c8acf717de2ea7b7eac8e8bdda
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-25 01:56:53 +0100

    Added onToggle method to onClick event handler

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -1,12 +1,15 @@
 import React from "react"
 
-const Toggle = () => {
+const Toggle = ({ onToggle }) => {
   return (
-    <div className="w-12 absolute right-0 top-0 mt-6 sm:mt-12 mr-6 sm:mr-12 lg:hidden cursor-pointer toggle">
+    <button
+      onClick={onToggle}
+      className="w-12 absolute right-0 top-0 mt-6 sm:mt-12 mr-6 sm:mr-12 lg:hidden cursor-pointer toggle"
+    >
       <span className="block py-1 mb-1 bg-theme"></span>
       <span className="block py-1 mb-1 bg-theme"></span>
       <span className="block py-1 mb-0 bg-theme"></span>
-    </div>
+    </button>
   )
 }
 
