30c1af2f8efb032395b9608851e64b7c169f925d
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-25 02:00:44 +0100

    Added toggleClass to navigation's set of classes

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,9 +1,13 @@
 import React from "react"
 import Link from "gatsby-link"
 
-const Navigation = () => {
+const Navigation = props => {
+  const { toggleClass } = { ...props }
+
   return (
-    <ul className="fixed bottom-0 top-0 w-3/4 h-full bg-green-500 -mx-12 sm:pl-12 pt-24 lg:w-full lg:h-auto lg:relative lg:visible lg:flex lg:bg-transparent lg:pt-0 lg:mx-0 theme-nav">
+    <ul
+      className={`fixed bottom-0 top-0 w-3/4 h-full bg-green-500 -mx-12 sm:pl-12 pt-24 lg:w-full lg:h-auto lg:relative lg:visible lg:flex lg:bg-transparent lg:pt-0 lg:mx-0 theme-nav ${toggleClass}`}
+    >
       <li className="ml-5 sm:ml-10 text-2xl sm:text-4xl tracking-tighter font-medium text-white lg:text-lg lg:font-normal lg:text-gray-900">
         <Link to="/">Home</Link>
       </li>
