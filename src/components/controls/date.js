38037797e37cc90e97f2440d6477c55b57855298
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-26 23:41:15 +0100

    Fixed className attribute

--- src/components/controls/date.js
+++ src/components/controls/date.js
@@ -3,7 +3,7 @@ import React from "react"
 const Date = ({ id, placeholder }) => {
   return (
     <input
-      class="w-full bg-primary text-secondary mt-3 p-4 rounded-lg tracking-tighter"
+      className="w-full bg-primary text-secondary mt-3 p-4 rounded-lg tracking-tighter theme-input"
       type="date"
       placeholder={placeholder}
       id={id}
