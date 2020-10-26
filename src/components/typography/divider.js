56e1d0357705bf5333985a1e80e9273733820381
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-26 23:44:03 +0100

    Created Divider component for form page on Report Crime

---
+++ src/components/typography/divider.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Divider = props => {
+  return (
+    <div className="w-full p-4 sm:px-8 sm:py-4 xxl:py-8 -mb-4 xxl:-mb-10 mt-4 xxl:-mt-2">
+      <h1 className="text-theme text-2xl tracking-tightest font-medium">
+        {props.children}
+      </h1>
+    </div>
+  )
+}
+
+export default Divider
