6009fdd35f3ae7e5e683f9b65d8b63037b264544
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-26 23:44:37 +0100

    Fixed className attribute

--- src/layout/container.js
+++ src/layout/container.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Container = props => {
   return (
-    <main class="w-full px-6 sm:px-10 pt-32 sm:pt-40 pb-16">
+    <main class="w-full px-6 sm:px-10 xxl:px-40 pt-32 sm:pt-40 xxl:pt-48 pb-16">
       {props.children}
     </main>
   )
