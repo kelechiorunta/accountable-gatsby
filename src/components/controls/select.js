d78c9da2bb2348cf5232202db706b3b1b63ac904
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-26 23:42:15 +0100

    Fixed className attribute

--- src/components/controls/select.js
+++ src/components/controls/select.js
@@ -4,7 +4,7 @@ const Select = ({ id, data }) => {
   return (
     <select
       id={id}
-      class="w-full bg-primary text-secondary mt-3 p-4 rounded-lg tracking-tighter"
+      className="w-full bg-primary text-secondary mt-3 p-4 rounded-lg tracking-tighter theme-input"
     >
       {Object.entries(data).map(([key, value], i) => (
         <option key={i} value={key}>
