6172470ded0a11ea880abfad723df3b2d0b53564
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-26 03:36:36 +0100

    Created button component

---
+++ src/components/controls/button.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ArrowIcon from "../icons/arrow-icon"
+
+const Button = props => {
+  return (
+    <button class="w-full bg-theme px-6 py-4 block rounded-lg text-white text-left tracking-tighter font-medium">
+      <span>{props.children}</span>
+      <div class="float-right">
+        <ArrowIcon />
+      </div>
+    </button>
+  )
+}
+
+export default Button
