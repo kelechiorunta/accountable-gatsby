cb1cf3795070dab1bec28570cce9df3ad1900b59
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-26 23:42:01 +0100

    Fixed className attribute

--- src/components/controls/multiline.js
+++ src/components/controls/multiline.js
@@ -4,7 +4,7 @@ const MultiLineText = ({ id, placeholder }) => {
   return (
     <textarea
       rows="8"
-      class="w-full bg-primary text-secondary mt-3 p-4 rounded-lg tracking-tighter"
+      className="w-full bg-primary text-secondary mt-3 p-4 rounded-lg tracking-tighter theme-input"
       id={id}
       placeholder={placeholder}
     ></textarea>
