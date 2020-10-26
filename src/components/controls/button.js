2db7a8031e8fa8036bbe278f0cdcc75d97e8512a
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-26 23:40:58 +0100

    Created button component

--- src/components/controls/button.js
+++ src/components/controls/button.js
@@ -3,7 +3,10 @@ import ArrowIcon from "../icons/arrow-icon"
 
 const Button = props => {
   return (
-    <button class="w-full bg-theme px-6 py-4 block rounded-lg text-white text-left tracking-tighter font-medium">
+    <button
+      id={props.id}
+      className="w-full bg-theme px-6 py-4 block rounded-lg text-white text-left tracking-tighter font-medium"
+    >
       <span>{props.children}</span>
       <div class="float-right">
         <ArrowIcon />
