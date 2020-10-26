743f830135bc086af3d9e78a4dee8c047e6aa83b
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-26 23:43:38 +0100

    Fixed className attribute

--- src/components/typography/caption.js
+++ src/components/typography/caption.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Caption = props => {
   return (
-    <h1 class="text-theme text-4xl font-medium leading-tight tracking-tightest">
+    <h1 className="text-theme text-4xl xxl:text-6xl font-medium leading-tight tracking-tightest">
       {props.children}
     </h1>
   )
