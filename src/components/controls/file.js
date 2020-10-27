02def31119fc08996059920ca0c813a315e47ac2
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-27 01:10:50 +0100

    Added logic for selecting file and updating value

--- src/components/controls/file.js
+++ src/components/controls/file.js
@@ -1,17 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
 
 const File = ({ id, placeholder }) => {
+  const [uploadedFile, setUploadedFile] = useState("Upload File")
+  const [uploadStyle, setUploadStyle] = useState("text-tertiary")
+
+  const onSelect = e => {
+    setUploadedFile(e.target.value)
+    setUploadStyle("text-secondary")
+  }
+
   return (
     <>
       <div className="relative">
         <input
+          onChange={onSelect}
           className=" theme-file"
           type="file"
           placeholder={placeholder}
           id={id}
         />
-        <div className="w-full bg-primary text-tertiary mt-3 p-4 rounded-lg tracking-tighter border-green-500 border-2 border-dashed">
-          Upload File
+        <div
+          className={`${uploadStyle} w-full bg-primary mt-3 p-4 rounded-lg tracking-tighter border-green-500 border-2 border-dashed`}
+        >
+          {uploadedFile}
         </div>
       </div>
     </>
