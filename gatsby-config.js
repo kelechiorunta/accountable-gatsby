32742fd1aaeb825d21b9871328cca2a51c285171
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-27 04:03:33 +0100

    Removed plugin for htaccess and fixed DM Sans font

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,10 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-postcss`,
-    `gatsby-plugin-htaccess`,
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          `dm sans\:400,400i,500,500i,700,700i`, // you can also specify font weights and styles
-        ],
+        fonts: [`DM Sans\:400,400i,500,500i,700,700i`],
         display: "swap",
       },
     },
