19684f3dc55fa8bdd266bf86b6a26b37fffc804e
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-27 01:17:40 +0100

    Made adjustments to container for breakpoints

--- src/components/header/navigation.js
+++ src/components/header/navigation.js
@@ -8,7 +8,7 @@ const Navigation = props => {
     <ul
       className={`fixed bottom-0 top-0 w-3/4 h-full bg-green-500 -mx-12 sm:pl-12 pt-24 lg:w-full lg:h-auto lg:relative lg:visible lg:flex lg:bg-transparent lg:pt-0 lg:mx-0 theme-nav ${toggleClass}`}
     >
-      <li className="ml-5 sm:ml-10 xxl:ml-12 text-2xl sm:text-4xl tracking-tighter font-medium text-white lg:text-lg lg:font-normal lg:text-gray-900">
+      <li className="ml-5 sm:ml-10 xl:ml-20 xxl:ml-12 text-2xl sm:text-4xl tracking-tighter font-medium text-white lg:text-lg lg:font-normal lg:text-gray-900">
         <Link to="/">Home</Link>
       </li>
       <li className="ml-5 sm:ml-10 xxl:ml-12 text-2xl sm:text-4xl tracking-tighter font-medium text-white lg:text-lg lg:font-normal lg:text-gray-900">
