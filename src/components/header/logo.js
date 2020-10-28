0166134a70194295010fb6cac24b4500e5b07918
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-28 12:12:33 +0100

    Modified Link import

--- src/components/header/logo.js
+++ src/components/header/logo.js
@@ -1,15 +1,18 @@
 import React from "react"
+import { Link } from "gatsby"
 import LogoImage from "../../images/logo.png"
 
 const Logo = () => {
   return (
     <>
-      <img
-        src={LogoImage}
-        width="50"
-        className="absolute top-0 left-0 ml-6 sm:ml-10 xl:ml-20 xxl:ml-40 mt-4 sm:mt-8 xxl:mt-12"
-        alt="Logo"
-      />
+      <Link to="/">
+        <img
+          src={LogoImage}
+          width="50"
+          className="absolute top-0 left-0 ml-6 sm:ml-10 xl:ml-20 xxl:ml-40 mt-4 sm:mt-8 xxl:mt-12"
+          alt="Logo"
+        />
+      </Link>
     </>
   )
 }
