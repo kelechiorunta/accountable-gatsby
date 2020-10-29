d4cc67ad38ff5a0e664bb5abf2c06e80420535df
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-29 23:53:40 +0100

    Added animation and imported necessary components

--- src/pages/crime.js
+++ src/pages/crime.js
@@ -4,18 +4,21 @@ import Container from "../layout/container"
 
 import Caption from "../components/typography/caption"
 import Summary from "../components/typography/summary"
+import Divider from "../components/typography/divider"
 
 import TextGroup from "../components/form-groups/text"
 import MultiLineGroup from "../components/form-groups/multiline"
+import Record from "../components/cards"
+import Label from "../components/controls/label"
 
 const CrimeDetails = () => {
   return (
     <Layout>
-      <Container>
+      <Container animation="theme-animate">
         <Caption>Jimoh Isiaq</Caption>
         <Summary>See all crimes as reported by Nigerians nationwide...</Summary>
-        <div className="w-full flex flex-wrap flex-col sm:flex-row">
-          <div className="w-full xl:w-3/5 xxl:w-4/5">
+        <div className="w-full flex flex-col-reverse lg:flex-row">
+          <div className="w-full xl:w-3/4 xxl:w-3/4">
             <div className="flex flex-wrap flex-col sm:flex-row -mx-4 sm:-mx-8">
               <div className="w-full sm:w-1/2 p-4 sm:px-8 sm:py-4 xxl:py-6">
                 <TextGroup
@@ -72,9 +75,63 @@ const CrimeDetails = () => {
                   placeholder="No.1 Surulere, Lagos"
                 />
               </div>
+
+              <div className="w-full sm:w-1/2 p-4 sm:px-8 sm:py-4 xxl:py-6">
+                <TextGroup
+                  id="report-where"
+                  name="Place, report was made"
+                  placeholder="No. 1 Obalande Street, Lagos."
+                />
+              </div>
+
+              <div className="w-full sm:w-1/2 p-4 sm:px-8 sm:py-4 xxl:py-6">
+                <TextGroup
+                  id="report-when"
+                  name="Date, report was made"
+                  placeholder="20 October, 2020"
+                />
+              </div>
+
+              <div className="w-full sm:w-1/2 p-4 sm:px-8 sm:py-4 xxl:py-6">
+                <TextGroup
+                  id="evidence"
+                  name="Evidence (Attachments)"
+                  placeholder="evidence-gunshot-1.jpg"
+                />
+              </div>
             </div>
           </div>
-          <div className="w-full xl:w-2/5 xxl:w-1/5">Ok</div>
+          <div className="w-full xl:w-1/4 xxl:w-1/4 lg:pl-8">
+            <Label>Photo</Label>
+            <div className="bg-primary h-64 rounded-xl mt-3"></div>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap flex-col sm:flex-row -mx-4 sm:-mx-8">
+          <Divider>Other Related Crimes</Divider>
+          <Record
+            victim="Jimoh Isiaq"
+            offender="CSP James Nwafor"
+            crime="Manslaugther"
+          />
+
+          <Record
+            victim="Chinyere Okafor"
+            offender="Inspector Akpan Kufre"
+            crime="Theft"
+          />
+
+          <Record
+            victim="Adebayo Emmanuel"
+            offender="CSP Akeredolu Akinlabi"
+            crime="Manslaugther"
+          />
+
+          <Record
+            victim="Felix Akande"
+            offender="CSP Caroline Afegbai"
+            crime="Manslaugther"
+          />
         </div>
       </Container>
     </Layout>
