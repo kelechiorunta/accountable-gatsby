d9c64179f4abf8e308b343d4de93e222a1f7248c
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-29 23:52:17 +0100

    Centralized logo

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -2,7 +2,7 @@ import React from "react"
 import LogoImage from "../../images/icon.png"
 
 const LogoIcon = () => {
-  return <img src={LogoImage} alt="Logo Icon" />
+  return <img src={LogoImage} alt="Logo Icon" className="mx-auto" />
 }
 
 export default LogoIcon
