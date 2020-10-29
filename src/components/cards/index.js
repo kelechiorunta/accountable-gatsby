13c7bbb692ad150ebf5d34f96b459480edd84fe0
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-29 23:50:22 +0100

    Modified Record card component

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -4,7 +4,7 @@ import LogoIcon from "../icons/logo"
 
 const Record = ({ victim, offender, crime }) => {
   return (
-    <div className="w-full md:w-1/2 lg:w-1/3 xxl:w-1/4 p-4 sm:p-8 mt-10">
+    <div className="w-full md:w-1/2 lg:w-1/3 xxl:w-1/4 p-4 sm:p-8 xxl:p-8 mt-10">
       <Link to="/crime">
         <div className="bg-primary text-secondary w-full py-16 px-3 text-center rounded-xl relative">
           <div className="theme-icon w-full flex justify-center absolute">
