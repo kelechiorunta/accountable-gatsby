5aee8940b2d9a08ecc25a5e45c6eaa277fbc5bfa
chigozieorunta <chigozieorunta@yahoo.com>
2020-10-29 23:53:04 +0100

    Added animation props to Container component

--- src/layout/container.js
+++ src/layout/container.js
@@ -2,7 +2,9 @@ import React from "react"
 
 const Container = props => {
   return (
-    <main class="w-full px-6 sm:px-10 xl:px-20 xxl:px-40 pt-32 sm:pt-40 xxl:pt-48 pb-16">
+    <main
+      class={`w-full px-6 sm:px-10 xl:px-20 xxl:px-40 pt-32 sm:pt-40 xxl:pt-48 pb-16 ${props.animation}`}
+    >
       {props.children}
     </main>
   )
